01788544790e4e0210af3a02f2ab68c598ba4163
浮中有夸 <fzyk.dylj@foxmail.com>
2023-01-10 13:19:06 +0800

    尚

---
+++ 02_src_hello/components/Hello/Hello.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Hello.css'
+
+export default class Hello extends Component {
+  render () {
+    return (
+      <div>
+        <h2 className='title'>Hello</h2>
+      </div>
+    )
+  }
+}
